9641593eb706058821098c700ddbfadbc512cdf3
Hanan Mehmood <hanan.mehmood@ymail.com>
2023-12-12 03:28:23 +0500

    Add theme for styled-components

---
+++ src/styles/theme.ts
@@ -0,0 +1,17 @@
+export const theme = {
+  colors: {
+    white: "#FFFFFF",
+    black: "#000000",
+    grey: {
+      100: "#F2F2F2",
+      300: "#C9C9C9",
+      500: "#8C8C8C",
+      900: "#323031",
+    },
+    primary: {
+      100: "#E0F5FF",
+      500: "#007BFF",
+      900: "#0B48A0",
+    },
+  },
+};
